1d74aab38c485677e73953d1599224f32ffd8453
apudebnath <apu1578@gmail.com>
2021-12-19 00:25:01 +0600

    content updated

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -7,13 +7,5 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
- /*  const firebaseConfig = {
-    apiKey: "AIzaSyD_MXYUF0vdmZyvE5XZeYTNudPrdhC9ZpI",
-    authDomain: "daily-yoga-a.firebaseapp.com",
-    projectId: "daily-yoga-a",
-    storageBucket: "daily-yoga-a.appspot.com",
-    messagingSenderId: "278358772176",
-    appId: "1:278358772176:web:d44dca637449b40de251d5"
-  }; */
 
 export default firebaseConfig;
