63e5bd3e6fe67ab29c60f04460713db31b1727d3
apudebnath <apu1578@gmail.com>
2021-12-19 11:23:27 +0600

    content uploaded

--- src/components/Home/Gallary/Gallary.js
+++ src/components/Home/Gallary/Gallary.js
@@ -2,6 +2,13 @@ import { Container, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 const image1 = ('https://i.ibb.co/dGNRZQd/image4.jpg');
+const image2 = ('https://i.ibb.co/P9FTMxS/image5.jpg');
+const image3 = ('https://i.ibb.co/c8hVmH8/image6.jpg');
+const image4 = ('https://i.ibb.co/VM8DD25/image7.jpg');
+const image5 = ('https://i.ibb.co/fxHwZNr/image8.jpg');
+const image6 = ('https://i.ibb.co/Fqk05bx/image9.jpg');
+const image7 = ('https://i.ibb.co/kxrTK8B/image10.jpg');
+const image8 = ('https://i.ibb.co/WPMwLNh/image11.jpg');
 const Gallary = () => {
     return (
         <Container sx={{my: 12}}>
@@ -15,37 +22,37 @@ const Gallary = () => {
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image2} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image3} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image4} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image5} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image6} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image7} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     <Grid item xs={3} sm={3} md={3} >
                         <Typography>
-                            <img src={image1} alt="" style={{width: '100%', borderRadius: '5px'}}/>
+                            <img src={image8} alt="" style={{width: '100%', borderRadius: '5px'}}/>
                         </Typography>
                     </Grid>
                     
